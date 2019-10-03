f5396c2d36ea416a9bd11f0b0c22b09bd62c6279
André Gusmão <agusmao@infoagesistemas.com.br>
2019-10-03 17:02:45 -0300

    second part of React JS class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import './App.css';
+import Routes from './routes';
 
 import logo from './assets/logo.svg';
 
-function App() {
+const App = () => {
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnc" />
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-        <form>
-          <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            id="email"
-            placeholder="seu melhor e-mail" />
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
